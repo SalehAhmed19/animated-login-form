624087452e11f32f973a4eea11909d5ce9a554b0
SalehAhmed19 <saleh.ahmed.mahin@gmail.com>
2022-10-28 08:08:02 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import * as Components from "./Components/Components";
 
 function App() {
+  const [signIn, toggle] = useState(true);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Components.Container>
+        <Components.SignUpContainer signingIn={signIn}>
+          <Components.Form>
+            <Components.Title>Create Account</Components.Title>
+            <Components.Input type="text" placeholder="Name" />
+            <Components.Input type="email" placeholder="Email" />
+            <Components.Input type="password" placeholder="Password" />
+            <Components.Button>Sign Up</Components.Button>
+          </Components.Form>
+        </Components.SignUpContainer>
+        <Components.SignInContainer signingIn={signIn}>
+          <Components.Form>
+            <Components.Title>Sign in</Components.Title>
+            <Components.Input type="email" placeholder="Email" />
+            <Components.Input type="password" placeholder="Password" />
+            <Components.Anchor href="#">
+              Forgot your password?
+            </Components.Anchor>
+            <Components.Button>Sign In</Components.Button>
+          </Components.Form>
+        </Components.SignInContainer>
+        <Components.OverlayContainer signingIn={signIn}>
+          <Components.Overlay signingIn={signIn}>
+            <Components.LeftOverlayPanel signingIn={signIn}>
+              <Components.Title>Welcome Back!</Components.Title>
+              <Components.Paragraph>
+                Please login with your personal info
+              </Components.Paragraph>
+              <Components.GhostButton onClick={() => toggle(true)}>
+                Sign In
+              </Components.GhostButton>
+            </Components.LeftOverlayPanel>
+            <Components.RightOverlayPanel signingIn={signIn}>
+              <Components.Title>Hello, Friend!</Components.Title>
+              <Components.Paragraph>
+                Enter your personal details and start journey with us
+              </Components.Paragraph>
+              <Components.GhostButton onClick={() => toggle(false)}>
+                Sign Up
+              </Components.GhostButton>
+            </Components.RightOverlayPanel>
+          </Components.Overlay>
+        </Components.OverlayContainer>
+      </Components.Container>
     </div>
   );
 }
 
 export default App;
+
+// package: npm install --save styled-components
